3babf83569018061b446d7a6e5f540dd500642b2
JohanGarcia03 <johan.garcia15@correo.tdea.edu.co>
2025-03-04 00:00:06 -0500

    Puertos e interfaces

---
+++ cs2/src/main/java/app/domain/services/AdminService.java
@@ -0,0 +1,16 @@
+package app.domain.services;
+
+import app.domain.models.Person;
+import app.domain.models.User;
+
+public class GuestService {
+
+    private PersonPort personPort;
+
+    public void RegisterVeterAndvendor (User admin){
+        if(PersonPort.exisPerson());
+
+
+    }
+        
+}
